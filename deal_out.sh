db3213e32689479a0e0e68782c146a8662d2c830
Spider391Tang <spider391Tang@gmail.com>
2013-03-30 14:05:51 +0800

    Delete unrelated files

---
+++ deal_out.sh
@@ -0,0 +1,23 @@
+#sed -i 's/ 1/台北市/g' out.csv
+#sed -i 's/ 3/台中市/g' out.csv
+#sed -i 's/ 5/台南市/g' out.csv
+#sed -i 's/ 7/高雄市/g' out.csv
+#sed -i 's/11/基隆市/g' out.csv
+sed -i 's/12/新竹市/g' out.csv
+sed -i 's/22/嘉義市/g' out.csv
+sed -i 's/31/新北市/g' out.csv
+sed -i 's/32/桃園縣/g' out.csv
+sed -i 's/33/新竹縣/g' out.csv
+sed -i 's/34/宜蘭縣/g' out.csv
+sed -i 's/35/苗栗縣/g' out.csv
+sed -i 's/37/彰化縣/g' out.csv
+sed -i 's/38/南投縣/g' out.csv
+sed -i 's/39/雲林縣/g' out.csv
+sed -i 's/40/嘉義縣/g' out.csv
+sed -i 's/43/屏東縣/g' out.csv
+sed -i 's/44/澎湖縣/g' out.csv
+sed -i 's/45/花蓮縣/g' out.csv
+sed -i 's/46/臺東縣/g' out.csv
+sed -i 's/46/台東縣/g' out.csv
+sed -i 's/90/金門縣/g' out.csv
+sed -i 's/91/連江縣/g' out.csv
